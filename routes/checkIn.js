4fe82eb046eb4a56ca5f3f4ef5bbbd31caaf0d67
John Seyfert <johnseyfert@gmail.com>
2017-11-30 22:31:50 -0700

    checkin route almost complete

--- routes/checkIn.js
+++ routes/checkIn.js
@@ -1,3 +1,4 @@
+//account for a general error in casea the site changes
 var express = require('express');
 var router = express.Router();
 const axios = require("axios");
@@ -6,48 +7,106 @@ router.get('/', function(req, res, next) {
 
 var firstName = 'john'
 var lastName = 'seyfert'
-var confirmationNumber = 'LNUF87'
+var confirmationNumber = 'MWVE2L'
 var emailAddress = 'johnseyfert@gmail.com'
+// var confirmationNumber = 'LNUF87' // not found
+
+const headers = {headers: {'Content-Type': 'application/json', 'X-API-Key': 'l7xx944d175ea25f4b9c903a583ea82a1c4c', } }
 
 const url1 = "https://www.southwest.com/air/check-in/index.html";
-// headers: { 'Content-Type': 'application/json' },
 
-const testUrl2 = "https://maps.googleapis.com/maps/api/geocode/json?address=austin";
-const testUrl3 = "https://maps.googleapis.com/maps/api/geocode/json?address=boston";
-const testUrl4 = "https://maps.googleapis.com/maps/api/geocode/json?address=denver";
-const testUrl5 = "https://maps.googleapis.com/maps/api/geocode/json?address=missoula";
+const url2 = "https://www.southwest.com/api/air-checkin/v1/air-checkin/page/air/check-in/review";
+// const data2 = JSON.stringify({
+//   confirmationNumber:confirmationNumber, 
+//   passengerFirstName:firstName, 
+//   passengerLastName:lastName, 
+//   site:"southwest"
+// })
 
-axios.get(testUrl2)
+
+const url3 = "https://www.southwest.com/api/air-checkin/v1/air-checkin/page/air/check-in/confirmation";
+// const data3 = JSON.stringify({
+//   airportCheckInRequiredReservation:false,
+//   confirmationNumber: confirmationNumber,
+//   drinkCouponSelected:false,
+//   electronicSystemTravelAuthorizationRequiredReservation:false,
+//   international:false,
+//   reprint:false,
+//   token:token,
+//   travelAuthorizationCheckNotPerformed:false,
+//   travelerIdentifiers:[],
+//   site:"southwest"
+// })
+
+const url4 = "https://www.southwest.com/api/air-checkin/v1/air-checkin/feature/mobile-boarding-pass";
+// const data4 = JSON.stringify({
+//   deliveryMethod:"EMAIL",
+//   destination:emailAddress,
+//   confirmationNumber:confirmationNumber,
+//   drinkCouponSelected:false,
+//   token:token,
+//   site:"southwest",
+// })
+
+
+axios.get(url1)
   .then((response) => {
-    console.log(
-      `City: ${response.data.results[0].formatted_address} -`,
-      `Latitude: ${response.data.results[0].geometry.location.lat} -`,
-      `Longitude: ${response.data.results[0].geometry.location.lng}`
-    );
-    return axios.get(testUrl3);
+    console.log('working1');
+
+    const data2 = JSON.stringify({
+      confirmationNumber:confirmationNumber, 
+      passengerFirstName:firstName, 
+      passengerLastName:lastName, 
+      site:"southwest"
+    })
+
+    return axios.post(url2, data2, headers)
+    
   })
   .then((response) => {
-    console.log(
-      `City: ${response.data.results[0].formatted_address} -`,
-      `Latitude: ${response.data.results[0].geometry.location.lat} -`,
-      `Longitude: ${response.data.results[0].geometry.location.lng}`
-    );
-    return axios.get(testUrl4);
+    console.log('working22');
+    let token = response.data.data.searchResults.token
+    console.log('token',token);
+
+    const data3 = JSON.stringify({
+      airportCheckInRequiredReservation:false,
+      confirmationNumber: confirmationNumber,
+      drinkCouponSelected:false,
+      electronicSystemTravelAuthorizationRequiredReservation:false,
+      international:false,
+      reprint:false,
+      token:token,
+      travelAuthorizationCheckNotPerformed:false,
+      travelerIdentifiers:[],
+      site:"southwest"
+    })
+
+    return axios.post(url3, data3, headers)
+
   })
   .then((response) => {
-    console.log(
-      `City: ${response.data.results[0].formatted_address} -`,
-      `Latitude: ${response.data.results[0].geometry.location.lat} -`,
-      `Longitude: ${response.data.results[0].geometry.location.lng}`
-    );
-    return axios.get(testUrl5);
+    console.log('working333');
+    let token = response.data.data.searchResults.token
+    console.log('token',token);
+
+    const data4 = JSON.stringify({
+      deliveryMethod:"EMAIL",
+      destination:emailAddress,
+      confirmationNumber:confirmationNumber,
+      drinkCouponSelected:false,
+      token:token,
+      site:"southwest",
+    })
+
+    return axios.post(url4, data4, headers)
+
   })
   .then((response) => {
-    console.log(
-      `City: ${response.data.results[0].formatted_address} -`,
-      `Latitude: ${response.data.results[0].geometry.location.lat} -`,
-      `Longitude: ${response.data.results[0].geometry.location.lng}`
-    );
+    console.log('working4444');
+    console.log('response.data', response.data);
+  })
+  .catch(error => {
+      console.log(error.response.data.notifications.formErrors[0].code)
   });
     
 });
@@ -55,48 +114,54 @@ axios.get(testUrl2)
 module.exports = router;
 
 
-// yield scrapy.FormRequest(
-//       "https://www.southwest.com/api/air-checkin/v1/air-checkin/page/air/check-in/review", 
-//       method="POST",
-//       headers={
-//       'Content-Type': 'application/json',
-//       'X-API-Key': 'l7xx944d175ea25f4b9c903a583ea82a1c4c'},
-//       body='{"confirmationNumber":"' + self.confirmationNumber + '","passengerFirstName":"' + self.firstName + '","passengerLastName":"' + self.lastName + '","site":"southwest"}',
-//       callback=self.parse2)
+// const testUrl2 = "https://maps.googleapis.com/maps/api/geocode/json?address=austin";
+// const testUrl3 = "https://maps.googleapis.com/maps/api/geocode/json?address=boston";
+// const testUrl4 = "https://maps.googleapis.com/maps/api/geocode/json?address=denver";
+// const testUrl5 = "https://maps.googleapis.com/maps/api/geocode/json?address=missoula";
 
 
+// console.log(
+//   `City: ${response.data.results[0].formatted_address} -`,
+//   `Latitude: ${response.data.results[0].geometry.location.lat} -`,
+//   `Longitude: ${response.data.results[0].geometry.location.lng}`
+// );
 
-// axios.get(testUrl1)
-// .then((response) => {
-//   console.log('working')
-//     axios.post(url2)
-//     .then((response) => {
-//         console.log(
-//           `City: ${response.data.results[0].formatted_address} -`,
-//           `Latitude: ${response.data.results[0].geometry.location.lat} -`,
-//           `Longitude: ${response.data.results[0].geometry.location.lng}`
-//         );
-//         axios.post(url3)
-//         .then((response) => {
-//             console.log(
-//               `City: ${response.data.results[0].formatted_address} -`,
-//               `Latitude: ${response.data.results[0].geometry.location.lat} -`,
-//               `Longitude: ${response.data.results[0].geometry.location.lng}`
-//             );
-//             axios.post(url4)
-//             .then((response) => {
-//                 console.log(
-//                   `City: ${response.data.results[0].formatted_address} -`,
-//                   `Latitude: ${response.data.results[0].geometry.location.lat} -`,
-//                   `Longitude: ${response.data.results[0].geometry.location.lng}`
-//                 );
-//             })
-//         })
+// const headers = {'Content-Type': 'application/json', 'X-API-Key': 'l7xx944d175ea25f4b9c903a583ea82a1c4c'}
+
+// SignIn = () => {
+//     console.log('login clicked')
+//     // let data = JSON.stringify({
+//     //     password: this.state.password,
+//     //     username: this.state.email
+//     // })
+//     let data = JSON.stringify({
+//       confirmationNumber:confirmationNumber, 
+//       passengerFirstName:firstName, 
+//       passengerLastName:lastName, 
+//       site:"southwest"
 //     })
-// })
 
+//     axios.post(url2, data, {
+//         headers: {
+//             'Content-Type': 'application/json',
+//             'X-API-Key': 'l7xx944d175ea25f4b9c903a583ea82a1c4c',
+//         }
+//     }
+//     )
+//       .then((response) => {
+//         console.log('asdfasfdasfasdfasdfasdfadsf');
+//         // console.log('headers', headers);
+//         // console.log('data', data);
+//         // console.log(
+//         //   `City: ${response.data.results[0].formatted_address} -`,
+//         //   `Latitude: ${response.data.results[0].geometry.location.lat} -`,
+//         //   `Longitude: ${response.data.results[0].geometry.location.lng}`
+//         // );
+//         // return axios.post(url2, data, headers);
+//       })
+//         .catch(error => {
+//             console.log(error.response.data.notifications.formErrors[0].code)
+//         });
+// }
 
-
-
-
-
+// SignIn()
